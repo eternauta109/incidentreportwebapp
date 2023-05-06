5c83272ce183759a9393ba1ea3416ba13e5d44bf
fabio conti <82828535+eternauta109@users.noreply.github.com>
2023-05-06 12:18:52 +0200

    Update Views.js

--- src/features/Views.js
+++ src/features/Views.js
@@ -4,7 +4,7 @@ import { getAllReports, getCinemaReports } from "../store/slice/reportsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import LineTable from "./LineTable";
 import Table from "react-bootstrap/Table";
-/* import Chart from "./Chart"; */
+import Chart from "./Chart"; 
 /* import ExcelImport from "./ExcelImport"; */
 import ToExcel from "./ToExcel";
 import SecondView from "./SecondView";
@@ -262,7 +262,7 @@ export default function View() {
         {/* <ExcelImport /> */}
       </Box>
 
-      {/* {listReport.length > 1 && <Chart data={listReport} />} */}
+      {listReport.length > 1 && <Chart data={listReport} />}
     </>
   );
 }
