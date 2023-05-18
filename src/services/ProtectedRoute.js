8c1ebecff5fc8f0bf281010ac614164ffcc3b890
eternauta109 <e_ternauta@live.it>
2023-05-18 13:48:44 +0200

    refactoring One

--- src/services/ProtectedRoute.js
+++ src/services/ProtectedRoute.js
@@ -1,13 +1,20 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux"; // un hook personalizzato che usa Firebase auth
 
-function ProtectedRoute({ children }) {
+function ProtectedRoute({ children, redirectTo }) {
   const user = useSelector((state) => state.user);
   const navigate = useNavigate();
-  if (!user) {
-    return navigate("/");
-  }
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!user) {
+      console.error("l'utente non è autorizzato");
+      if (location.pathname !== redirectTo) {
+        return navigate(redirectTo);
+      }
+    }
+  }, [user, navigate, redirectTo]);
 
   return children;
 }
